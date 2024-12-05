2defdd7538ae6ecfc3966de05a6d6e09d0527582
Jared Williams <jaredvwilliams@gmail.com>
2024-12-04 22:01:13 -0800

    Make dev script use local package

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,22 @@
-export default {
+import { defineConfig } from 'vite'
+
+export default defineConfig(({ command, mode }) => {
+  const config = {
     base: '/webgl-fluid',
     server: {
-        host: '0.0.0.0',
-        port: 3001,
+      host: '0.0.0.0',
+      port: 3001,
     }
-}
+  }
+
+  if (mode === 'local-dev') {
+    // Use the local version of the package instead of the published version
+    config.resolve = {
+      alias: {
+        '@red_j/webgl-fluid-sim': '/src/webgl-fluid-sim/src'
+      }
+    }
+  }
+
+  return config
+})
